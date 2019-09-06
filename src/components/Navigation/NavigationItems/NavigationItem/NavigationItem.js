398a8c644835e2970046b6f883ad3368a48ecaa8
Anvar <Anvar-abd_97@mail.ru>
2019-09-06 23:52:17 +0500

    responsive nav

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,23 +2,18 @@ import React from "react";
 import classes from "./NavigationItem.module.css";
 import { NavLink } from "react-router-dom";
 const navigationItem = props => {
-  const navigationItemClasses = [
-    classes.NavigationItem,
-    props.vertical ? classes.Vertical : classes.Horizontal
-  ];
-  return (
-    <li className={navigationItemClasses.join(" ")}>
-      <NavLink
-        onClick={props.drawerClosed}
-        to={props.link}
-        activeClassName={
-          props.vertical ? classes.ActiveSideDrawerLink : classes.ActiveLink
-        }
-      >
-        {props.children}
-      </NavLink>
-    </li>
-  );
+	const navigationItemClasses = [ classes.NavigationItem, props.vertical ? classes.Vertical : classes.Horizontal ];
+	return (
+		<li className={navigationItemClasses.join(" ")}>
+			<NavLink
+				onClick={props.drawerClosed}
+				to={props.link}
+				activeClassName={props.vertical ? classes.ActiveSideDrawerLink : classes.ActiveLink}
+			>
+				{props.children}
+			</NavLink>
+		</li>
+	);
 };
 
 export default navigationItem;
